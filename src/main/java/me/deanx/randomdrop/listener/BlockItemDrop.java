6aa5c861cd95cc3d75ea3275d4a77b58d96ca6a6
xztdean <xztdean@gmail.com>
2021-09-29 03:13:58 -0700

    Avoid deprecated items

--- src/main/java/me/deanx/randomdrop/listener/BlockItemDrop.java
+++ src/main/java/me/deanx/randomdrop/listener/BlockItemDrop.java
@@ -9,7 +9,9 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockDropItemEvent;
 import org.bukkit.inventory.ItemStack;
 
+import java.lang.reflect.Field;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 
@@ -17,6 +19,9 @@ public class BlockItemDrop implements Listener {
     private final HashMap<Material, Material> dropMap = new HashMap<>();
     private static final Material[] materials = Material.values();
     private final Random random = new Random();
+    private final HashSet<Material> unavailableItem = new HashSet<>(List.of(
+            Material.COMMAND_BLOCK, Material.COMMAND_BLOCK_MINECART, Material.CHAIN_COMMAND_BLOCK,
+            Material.REPEATING_COMMAND_BLOCK, Material.STRUCTURE_BLOCK, Material.STRUCTURE_VOID));
 
     public BlockItemDrop(Plugin plugin) {
         Bukkit.getPluginManager().registerEvents(this, plugin);
@@ -32,11 +37,28 @@ public class BlockItemDrop implements Listener {
                 Material material;
                 do {
                     material = materials[random.nextInt(materials.length)];
-                } while (!material.isItem() || dropMap.containsValue(material));
+                } while (!material.isItem() || unavailableItem.contains(material) || !isDeprecated(material));
                 dropMap.put(type, material);
+                unavailableItem.add(material);
             }
             ItemStack newItem = new ItemStack(dropMap.get(type), itemStack.getAmount());
             item.setItemStack(newItem);
+        }
+    }
+
+    private boolean isDeprecated(Material material) {
+        Field field;
+        try {
+            field = Material.class.getField(material.name());
+        } catch (NoSuchFieldException e) {
+            Bukkit.getLogger().warning("Find unkown Material " + material.name());
+            return true;
+        }
+        if (field.isAnnotationPresent(Deprecated.class)) {
+            unavailableItem.add(material);
+            return true;
+        } else {
+            return false;
         }
     }
 }
